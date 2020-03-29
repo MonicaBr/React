2d6c80fdccc797f2707d35efd1d20e9c67b2ae2e
Monica Breaban <monica.breaban@centric.eu>
2020-03-29 13:25:20 +0300

    Added Function and Component

---
+++ my-first-app/src/List.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            coffeeShops: ["Roast", "Status", "Cafeneaua Noastra", "Teo's"],
+            foodShops: ["Auchan", "Bruno", "Kung food"],
+            personalChoice: ""
+        };
+
+        this.personalChoiceClick = this.personalChoiceClick.bind(this);
+    }
+
+    personalChoiceClick() {
+        this.setState({ personalChoice: "Cafeneaua Noastra" });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Coffee shops</h3>
+                <div>{this.state.coffeeShops.map(i => <span>{i} </span>)}</div>
+                
+                <h3>Food shops</h3>
+                <div>{this.state.foodShops.map(i => <span>{i} </span>)}</div>
+                
+                <h3>All shops</h3>
+                <div>{ [...this.state.coffeeShops, ...this.state.foodShops].map(i => <span>{i} </span>) }</div>
+
+                <h4>Psst! Wanna know my personal choice of coffee?</h4>
+                <button onClick={this.personalChoiceClick}>Click here!</button>
+                <p hidden={this.state.personalChoice == ""}>{this.state.personalChoice}! Can you spell it? It's {[...this.state.personalChoice].map(i => <span>{i} </span>)}</p>
+            </div>
+        );
+    }
+}
+
+export default List;
+
+ReactDOM.render(
+    <List />,
+    document.getElementById('root')
+)
---
+++ my-first-app/src/Select.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from './List';
+
+function Select() {
+    return (
+      <div>
+        <h1>Welcome to Palas Mall! What would you like to enjot today?
+            Coffee? Food? Or why not both?
+        </h1>
+        <List />
+      </div>
+    );
+}
+
+export default Select;
